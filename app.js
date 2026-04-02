ea905d9e06a08c78655d60347f4de49899fd53b1
HeshamAmen <84250834+HeshamAmen@users.noreply.github.com>
2026-04-03 02:56:53 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+{
+  "name": "مجالس الصالحين",
+  "package": "com.saleheen.store",
+  "version": "2.0",
+  "icon": "🕌",
+  "orientation": "portrait",
+  "permissions": ["storage", "internet"],
+  "author": "مجالس الصالحين",
+  "description": "متجر التطبيقات الإسلامية"
+}
